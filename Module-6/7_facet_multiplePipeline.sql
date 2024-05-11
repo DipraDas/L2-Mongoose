d8204707d797d8884c0f7a18ff73ee50dd35c135
Dipra <dipradas5940@gmail.com>
2024-05-11 22:14:17 +0600

    [6.7] facet_multiPipeline

---
+++ Module-6/7_facet_multiplePipeline.sql
@@ -0,0 +1,20 @@
+db.test.aggregate([
+    {
+        $facet: {
+            // pipeline 1
+            'friendsCount': [
+                // stage 1
+                { $unwind: "$friends" },
+                // stage 2
+                { $group: { _id: "$friends", count: { $sum: 1 } } }
+            ],
+            // pipeline 2
+            'educationCount': [
+                //stage 1
+                { $unwind: "$education" },
+                //stage 2
+                { $group: { _id: "$education", count: { $sum: 1 } } }
+            ]
+        }
+    }
+])
